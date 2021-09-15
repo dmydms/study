41a58aa8d0af73aa13462bd24b58586833492442
dhdh3311 <dhdh3311@naver.com>
2021-09-16 01:20:59 +0900

    refactor: list comprehension을 이용해서 리팩토링

--- algorithm/bj10430.py
+++ algorithm/bj10430.py
@@ -1,24 +1,6 @@
-# TODO: map 이용해서 리팩토링하기
+a, b, c = [int(n) for n in input().split(' ')]
 
-def calculate_expression0(a: int, b: int, c: int):
-    return (a + b) % c
-
-def calculate_expression1(a: int, b: int, c: int):
-    return ((a % c) + (b % c)) % c 
-
-def calculate_expression2(a: int, b: int, c: int):
-    return (a * b) % c
-
-def calculate_expression3(a: int, b: int, c: int):
-    return ((a % c) * (b % c)) % c 
-
-a, b, c = input().split(' ')
-
-a = int(a)
-b = int(b)
-c = int(c)
-
-print(calculate_expression0(a, b, c))
-print(calculate_expression1(a, b, c))
-print(calculate_expression2(a, b, c))
-print(calculate_expression3(a, b, c))
+print((a + b) % c)
+print(((a % c) + (b % c)) % c)
+print((a * b) % c)
+print(((a % c) * (b % c)) % c)
