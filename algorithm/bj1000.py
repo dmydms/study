5fa36916ff79173454037c3129a8f48044b013eb
dhdh3311 <dhdh3311@naver.com>
2021-09-15 10:30:28 +0900

    feat: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램

---
+++ algorithm/bj1000.py
@@ -0,0 +1,11 @@
+# 마주친 에러: 
+# SyntaxError: unexpected EOF while parsing
+# python3이 아니라 python2 로 실행함 
+# python3에서 실행하는게 목적이였기 때문에 또 다시 에러를 마주쳤을 때 찾아보기로 함
+
+def sum(a, b): 
+    return a + b
+
+a, b = input().split(' ')
+
+print(sum(int(a), int(b)))
