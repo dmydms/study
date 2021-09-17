28bfd3685c74908899db082bef05345767c6c87c
dhdh3311 <dhdh3311@naver.com>
2021-09-18 00:07:50 +0900

    refactor: list comprehension을 사용해서 리팩토링

--- algorithm/bj10869.py
+++ algorithm/bj10869.py
@@ -1,8 +1,7 @@
-# 바로 형변환해서 a, b에 저장하는건 없나? 
-a, b = input().split(' ')
+# Q. 바로 형변환해서 a, b에 저장하는건 없나? 
+# A. List Comprehension
 
-a = int(a)
-b = int(b)
+a, b = [int(n) for n in input().split(' ')]
 
 print(a + b)
 print(a - b)
