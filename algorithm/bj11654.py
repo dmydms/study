60434cf82bfb298f90256494c01b66af40aacf9e
dhdh3311 <dhdh3311@naver.com>
2021-09-18 23:43:17 +0900

    feat: ASCII code 출력하기

---
+++ algorithm/bj11654.py
@@ -0,0 +1,4 @@
+# 배운 점: 
+# chr(), Return the string representing a character whose Unicode code point is the integer i.
+# ord(), Given a string representing one Unicode character, return an integer representing the Unicode code point of that character.
+print(ord(input()))
