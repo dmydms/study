ade7119bd35f6f45d609ac11654f683ea4373ce1
dhdh3311 <dhdh3311@naver.com>
2021-11-07 14:56:23 +0900

    refactor: Removing the `play` variable

--- 독서/Martin Fowler, Refactoring/chapter01.js
+++ 독서/Martin Fowler, Refactoring/chapter01.js
@@ -24,9 +24,9 @@ const plays = {
 }
 
 function statement(invoice, plays) {
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -41,11 +41,15 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result
     }
 
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+ 
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
@@ -56,16 +60,15 @@ function statement(invoice, plays) {
     }).format;
     
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        const thisAmount = amountFor(perf, play);
+        const thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0)
         // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // print line for this order
-        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
 
